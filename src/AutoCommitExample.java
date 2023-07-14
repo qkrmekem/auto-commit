5b0dd415e4fce0f943a356dff59fa1f25f8791cb
qkrmekem <magicofclown@naver.com>
2023-07-15 04:37:29 +0900

    자동 커밋 반영

--- src/AutoCommitExample.java
+++ src/AutoCommitExample.java
@@ -26,7 +26,7 @@ public class AutoCommitExample {
                     WatchEvent.Kind<?> kind = event.kind();
 
                     // 디렉토리 변경 이벤트일 경우에만 처리
-                    if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                    if (kind == StandardWatchEventKinds.ENTRY_MODIFY || kind == StandardWatchEventKinds.ENTRY_CREATE || kind == StandardWatchEventKinds.ENTRY_DELETE) {
                         // 변경된 디렉토리 경로
                         Path changedDirectory = ((Path) watchKey.watchable()).resolve((Path) event.context());
 
@@ -55,55 +55,12 @@ public class AutoCommitExample {
         Files.walkFileTree(directory, new SimpleFileVisitor<>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                dir.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+                dir.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
                 return FileVisitResult.CONTINUE;
             }
         });
     }
 
-//    private static void commitChangedFile(Path changedFile) {
-//        if (changedFile == null) {
-//            System.out.println("변경된 파일이 유효하지 않습니다.");
-//            return;
-//        }
-//
-//        try {
-//            String gitPath = "C:/Program Files/Git/cmd/git.exe";
-//            Path directoryPath = changedFile.getParent(); // 수정: String이 아닌 Path로 변경
-//
-//            // 변경된 파일의 디렉토리로 이동
-//            ProcessBuilder processBuilder = new ProcessBuilder(gitPath, "-C", directoryPath.toString(), "add", changedFile.toString());
-//            Process process = processBuilder.start();
-//            process.waitFor();
-//
-//            // 변경된 파일이 있는지 확인
-//            Process statusProcess = new ProcessBuilder(gitPath, "-C", directoryPath.toString(), "status", "--porcelain").start();
-//            BufferedReader statusReader = new BufferedReader(new InputStreamReader(statusProcess.getInputStream()));
-//            boolean hasChanges = statusReader.lines().count() > 0;
-//
-//            if (hasChanges) {
-//                // 커밋 메시지와 함께 커밋 수행
-//                processBuilder.command(gitPath, "-C", directoryPath.toString(), "commit", "-m", "자동 커밋");
-//
-//                // 현재 작업 중인 로컬 브랜치 확인
-//                Process branchProcess = new ProcessBuilder(gitPath, "-C", directoryPath.toString(), "rev-parse", "--abbrev-ref", "HEAD").start();
-//                BufferedReader branchReader = new BufferedReader(new InputStreamReader(branchProcess.getInputStream()));
-//                String currentBranch = branchReader.readLine();
-//
-//                // 현재 작업 중인 브랜치로 푸시
-//                processBuilder.command(gitPath, "-C", directoryPath.toString(), "push", "origin", currentBranch);
-//                process = processBuilder.start();
-//                process.waitFor();
-//
-//                System.out.println("자동 커밋이 수행되었습니다.");
-//            } else {
-//                System.out.println("변경된 내용이 없어 자동 커밋이 수행되지 않았습니다.");
-//            }
-//
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     private static void commitChangedFile(Path changedFile) {
         if (changedFile == null) {
--- src/backJoon/Test.java
+++ src/backJoon/Test.java
@@ -7,6 +7,19 @@ public class Test {
         System.out.println((int)Math.sqrt(30));
         // auto commit test
         // testtesetestsettst
-        System.out.println("test");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
