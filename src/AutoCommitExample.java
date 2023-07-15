2db443617fd7cad85995e841895119406fc4a194
qkrmekem <magicofclown@naver.com>
2023-07-15 09:53:27 +0900

    auto commit

--- src/AutoCommitExample.java
+++ src/AutoCommitExample.java
@@ -1,3 +1,5 @@
+import org.eclipse.jgit.api.Git;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,6 +8,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class AutoCommitExample {
     public static void main(String[] args) {
-
+        Git
     }
 }
